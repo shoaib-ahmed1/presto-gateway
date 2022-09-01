bc916e148c6337d1683ffea50bf9b0c68ecb9967
Shoaib Ahmed <109759323+shoaib-ahmed1@users.noreply.github.com>
2022-09-01 16:41:48 +0500

    Update run_dummy_queries.py

--- run_dummy_queries.py
+++ run_dummy_queries.py
@@ -2,32 +2,37 @@ from trino.dbapi import connect
 import sys
 
 def runDummyQueries(argv):
-    conn = connect(
-        host=str(argv[0]),
-        port=argv[1],
-        user="adhoc"
-    )
-    cur = conn.cursor()
 
-    list = []
-    queries = [
-        "select count(1) from(select * from tpch.tiny.part limit 1)",
-        "select count(1) from(select * from tpch.tiny.orders limit 1)",
-        # "select count(*) from tpch.tiny.supplier limit 1",
-        # "select count(*) from tpch.tiny.orders limit 1",
-    ]
+    try:
+        conn = connect(
+            host=str(argv[0]),
+            port=argv[1],
+            user="adhoc"
+        )
+        cur = conn.cursor()
 
-    for query in queries:
-        cur.execute(query)
-        rows = cur.fetchone()
-        # print(rows)
-        # print(cur.rowcount)
-        list.append(rows[0])
+        list = []
+        queries = [
+            "select count(1) from(select * from tpch.tiny.part limit 1)",
+            "select count(1) from(select * from tpch.tiny.orders limit 1)",
+            # "select count(*) from tpch.tiny.supplier limit 1",
+            # "select count(*) from tpch.tiny.orders limit 1",
+        ]
 
-    cur.close()
-    conn.close()
-    #print(list)
-    return (0 in list)
+        for query in queries:
+            cur.execute(query)
+            rows = cur.fetchone()
+            # print(rows)
+            # print(cur.rowcount)
+            list.append(rows[0])
+
+        cur.close()
+        conn.close()
+        #print(list)
+        return (0 in list)
+    except :
+        print("Unknown error occurred")
+        return False
 
 
 if __name__ == "__main__":
